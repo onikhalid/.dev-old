eceae73b3fb9d570d4297126161f65e4925149fb
Oni Khalid Ayòbámi <73073198+onikhalid@users.noreply.github.com>
2022-07-31 11:24:43 +0100

    Initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,169 @@
+///////////////////////////
+////////THEME SWITCH///////
+///////////////////////////
+const body = document.querySelector('body');
+const switcher = document.querySelector('.switch-input')
+const currentTheme = localStorage.getItem("theme")
+
+
+
+window.addEventListener('DOMContentLoaded', ()=>{
+  body.setAttribute('data-theme', currentTheme)
+})
+
+
+
+switcher.addEventListener('click', ()=>{
+  const currentDataTheme = body.getAttribute('data-theme')
+  if(currentDataTheme == 'light'){
+    body.setAttribute('data-theme', 'dark')
+    localStorage.setItem("theme", 'dark')
+  }else{
+    body.setAttribute('data-theme', 'light')
+    localStorage.setItem("theme", 'light')
+  }
+})
+
+
+
+const dataThemeItem = body.getAttribute('data-theme')
+const localThemeItem = localStorage.getItem("theme")
+if(dataThemeItem == '' && localThemeItem == ''){
+  body.setAttribute('data-theme', 'dark')
+  localStorage.setItem("theme", 'dark')
+}
+else if(dataThemeItem == '' && localThemeItem == 'dark'){
+  body.setAttribute('data-theme', 'dark')
+  localStorage.setItem("theme", 'dark')
+}
+else if(dataThemeItem == '' && localThemeItem == 'light'){
+  body.setAttribute('data-theme', 'light')
+  localStorage.setItem("theme", 'light')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////
+////////CUSTOM CURSOR///////
+///////////////////////////
+
+// Make shape follow cursor
+const cursor = document.getElementById("cursor");
+const anchors = document.querySelectorAll('a, button, label');
+
+onmousemove =  (mousePosition)=> {
+  const  pos = mousePosition;
+    document.addEventListener("mousemove", (mousePosition) => {
+      cursor.setAttribute(
+        "style",
+        "left : " + (pos.x - 15) + "px; top: " + (pos.y - 15) + "px;"
+      );
+    });
+    document.addEventListener("click", (mousePosition) => {
+      cursor.classList.add("clicked");
+      setTimeout(function () {
+      cursor.classList.remove("clicked");
+    }, 500);
+  });
+};
+
+
+// Expand cursor on link hover
+const links = Array.from(anchors)
+  links.forEach((anchor, index) =>{
+    anchor.addEventListener("mouseover", ()=>{
+      cursor.classList.add("hover");
+    }) 
+
+    anchor.addEventListener("mouseout", ()=>{
+      cursor.classList.remove("hover");
+    }) 
+});
+
+  
+
+
+
+
+
+//Get the button:
+topButton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    topButton.style.display = "block";
+  } else {
+    topButton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+topFunction = ()=> {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+
+
+
+///////////////////////////
+///////// NAV MENU/////////
+///////////////////////////
+const Ham = document.querySelector('.menu-hamburger');
+const nav = document.getElementById('header__nav')
+
+document.addEventListener('click', (event)=>{
+  const navClicked = nav.contains(event.target)
+  const hamClicked = Ham.contains(event.target)
+
+  if (!navClicked){
+
+      if(hamClicked){
+        if(Ham.classList.contains('open')){
+          Ham.classList.remove('open')
+          nav.classList.remove('open')
+          nav.classList.add('close')
+          
+        }
+        else{
+          Ham.classList.add('open')
+          nav.classList.remove('close')
+          nav.classList.add('open')
+        }
+      }
+      else{
+        if(Ham.classList.contains('open')){
+          Ham.classList.remove('open')
+          nav.classList.remove('open')
+          nav.classList.add('close')
+          
+        }
+        else{
+          
+        }
+      }
+    }
+
+  else {
+    Ham.classList.add('open')
+    nav.classList.remove('close')
+    nav.classList.add('open')
+  }
+
+})
+ 
